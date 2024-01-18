b263686e65a653e2756d75e337062db600d6ef8b
Alessandro Scandone <alescandone@gmail.com>
2024-01-18 01:33:40 +0100

    integrated lsp

--- src/extension.ts
+++ src/extension.ts
@@ -1,30 +1,46 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
+import { LanguageClient } from "vscode-languageclient/node";
+
+const executable = {
+  command: "mini-rs",
+  args: ["lsp", "--", "--stdio"],
+};
+
+const serverOptions = {
+  run: executable,
+  debug: executable,
+};
+
+const clientOptions = {
+  documentSelector: [
+    {
+      scheme: "file",
+      language: "mini-rs",
+    },
+  ],
+};
+
+const client = new LanguageClient(
+  "mini-rs-language-server",
+  "MiniRust language client",
+  serverOptions,
+  clientOptions
+);
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "mini-rust-lsp" is now active!');
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand(
-    "mini-rust-lsp.helloWorld",
-    () => {
-      // The code you place here will be executed every time your command is executed
-      // Display a message box to the user
-      vscode.window.showInformationMessage(
-        "Hello World from MiniRust Language Client!"
-      );
-    }
-  );
-
-  context.subscriptions.push(disposable);
+  client.start().catch((e) => {
+    console.error(e);
+  });
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+  client.dispose();
+}

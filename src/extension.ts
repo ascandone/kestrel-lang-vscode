571bfb9b53f951fa5aa38088bbd9c8a39278abc3
Alessandro Scandone <alescandone@gmail.com>
2024-03-15 22:15:43 +0100

    improved comments

--- src/extension.ts
+++ src/extension.ts
@@ -29,9 +29,17 @@ const client = new LanguageClient(
   clientOptions
 );
 
+let configureLang: vscode.Disposable | undefined;
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  const onEnterRules = [...continueTypingCommentsOnNewline()];
+
+  configureLang = vscode.languages.setLanguageConfiguration("kestrel", {
+    onEnterRules,
+  });
+
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
 
@@ -42,5 +50,33 @@ export function activate(context: vscode.ExtensionContext) {
 
 // This method is called when your extension is deactivated
 export function deactivate() {
+  configureLang?.dispose();
   client.dispose();
+}
+
+/**
+ * Returns the `OnEnterRule`s needed to configure typing comments on a newline.
+ *
+ * This makes it so when you press `Enter` while in a comment it will continue
+ * the comment onto the next line.
+ *
+ * Credits: https://github.com/gleam-lang/vscode-gleam/blob/main/src/extension.ts#L96
+ */
+function continueTypingCommentsOnNewline(): vscode.OnEnterRule[] {
+  const indentAction = vscode.IndentAction.None;
+
+  return [
+    {
+      // Module doc single-line comment
+      // e.g. ////|
+      beforeText: /^\s*\/{4}.*$/,
+      action: { indentAction, appendText: "//// " },
+    },
+    {
+      // Doc single-line comment
+      // e.g. ///|
+      beforeText: /^\s*\/{3}.*$/,
+      action: { indentAction, appendText: "/// " },
+    },
+  ];
 }

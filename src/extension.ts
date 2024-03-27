5c3e5261861ac90d1dbb4f6ba38e8af69ca85ce4
Alessandro Scandone <alescandone@gmail.com>
2024-03-27 11:08:02 +0100

    added restart server command

--- src/extension.ts
+++ src/extension.ts
@@ -3,6 +3,8 @@
 import * as vscode from "vscode";
 import { LanguageClient } from "vscode-languageclient/node";
 
+const RESTART_SERVER_COMMAND = "kestrel.restartServer";
+
 const executable = {
   command: "kestrel",
   args: ["lsp", "--", "--stdio"],
@@ -39,6 +41,30 @@ export function activate(context: vscode.ExtensionContext) {
   configureLang = vscode.languages.setLanguageConfiguration("kestrel", {
     onEnterRules,
   });
+
+  // Copied from:
+  // https://github.com/gleam-lang/vscode-gleam/blob/1a8cac7103f85e3e4e309190bb4d43ac1483cef9/src/extension.ts#L23
+  const restartCommand = vscode.commands.registerCommand(
+    RESTART_SERVER_COMMAND,
+    async () => {
+      if (!client) {
+        vscode.window.showErrorMessage("kestrel client not found");
+        return;
+      }
+
+      try {
+        if (client.isRunning()) {
+          await client.restart();
+
+          vscode.window.showInformationMessage("kestrel server restarted.");
+        } else {
+          await client.start();
+        }
+      } catch (err) {
+        client.error("Restarting client failed", err, "force");
+      }
+    }
+  );
 
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
